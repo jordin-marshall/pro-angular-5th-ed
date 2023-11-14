e6e15fa9a9e58b144bc006a97b7e42e811a264bb
jordin-marshall <61058317+jordin-marshall@users.noreply.github.com>
2023-11-14 12:57:47 -0500

    feat: added todo components and initial styling for todo list

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from './components/todo/todoList';
+import { TodoItem } from './components/todo/todoItem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get Flowers"),
+    new TodoItem("Collect Tickets"),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
+
 }
